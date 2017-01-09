d2b71f742428a260339141d0f42c3bbdff7e805c
Aditi Green <asaki444@gmail.com>
2017-01-09 20:34:26 +0000

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ return main;
 
 
   function increaseRankBy(n){
-
+  var val = parseInt(n);
     const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
    for (let i = 0, l = list.length; i < l; i++) {
-      list[i].innerHTML = parseInt(list[i+n].innerHTML);
+    list[i].innerHTML = parseInt(list[i].innerHTML) +n;
     }
   }
