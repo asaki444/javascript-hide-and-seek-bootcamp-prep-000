c3492af58962974dc281aaafd9ff92c579a7ce5c
Aditi Green <asaki444@gmail.com>
2017-01-09 16:57:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+ return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector("#nested");
+}
+
+function deepestChild(){
+const main =  document.querySelector("#grand-node")
+return main;
+
+}
+
+function increaseRankBy(n){
+
+  function increaseRankBy(n){
+
+    const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+   for (let i = 0, l = list.length; i < l; i++) {
+      list[i].innerHTML = parseInt(list[i+n].innerHTML);
+    }
+  }
+}
